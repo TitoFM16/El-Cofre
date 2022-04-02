b9828b97e7f7adf04cf10771a32f04105ab7d511
Titofm16 <juffigueroama@unal.edu.co>
2022-04-02 18:44:31 -0500

    agragado componente de whatsapp

---
+++ components/whatsappButton.js
@@ -0,0 +1,5 @@
+document.write('\
+<a href="https://api.whatsapp.com/send?phone=+573155806571&text=Buenos días, estoy interesado en la estación El nuevo cofre%21%20." class="floating" target="_blank">\
+    <i class="fa fa-whatsapp float-button"></i>\
+</a>\
+');
